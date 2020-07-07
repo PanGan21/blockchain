11bf6cd16a129ac8efc99e9cf895954632629758
Panagiotis Ganelis <panagiotisganelis@Panagiotiss-MacBook-Pro.local>
2020-07-07 14:12:15 +0200

    create structs for transaction blockheader and block

---
+++ src/blockchain.rs
@@ -0,0 +1,24 @@
+use serde::Serialize;
+
+#[derive(Debug, Clone, Serialize)]
+struct Transaction {
+    sender: String,
+    receiver: String,
+    amount: f32,
+}
+
+#[derive(Serialize, Debug)]
+pub struct Blockheader {
+    timestamp: i64,
+    nonce: u32,
+    pre_hash: String,
+    merkle: String,
+    difficulty: u32,
+}
+
+#[derive(Serialize, Debug)]
+pub struct Block {
+    header: Blockheader,
+    count: u32,
+    transactions: Vec<Transaction>,
+}
